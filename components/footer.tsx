cdf088a6bfc78bf8a9e1ca72ef139f7f0ac6a50c
ashcbrd <cabardoash@gmail.com>
2024-06-26 01:29:44 +0800

    style: change layout

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,16 @@
 /* eslint-disable @next/next/no-img-element */
+import { termsOfService } from "@/data/terms-of-service";
 import Logo from "./logo";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { privacyPolicy } from "@/data/privacy";
+import { cookiesPolicy } from "@/data/cookies";
 
 const Footer = () => {
   return (
@@ -8,14 +19,233 @@ const Footer = () => {
         <div className="w-max flex flex-col items-center tablet:items-start gap-y-8">
           <Logo />
           <ul className="space-y-4">
-            <li className="font-semibold text-lg flex items-center gap-x-2 cursor-pointer">
-              <img src="/right-arrow.png" className="w-4" /> Terms
+            <li>
+              <Dialog>
+                <DialogTrigger className="font-semibold text-lg flex items-center gap-x-2 cursor-pointer">
+                  <img src="/right-arrow.png" className="w-4" /> Terms
+                </DialogTrigger>
+                <DialogContent className="h-[90%] bg-white overflow-y-scroll">
+                  <DialogHeader>
+                    <DialogTitle className="text-black text-start font-bold text-2xl mb-10">
+                      TERMS OF SERVICE
+                    </DialogTitle>
+                    <DialogDescription>
+                      {termsOfService.map((item, index) => (
+                        <div
+                          key={index}
+                          className="text-black text-start h-max"
+                        >
+                          <h4 className="font-semibold">
+                            {index + 1}. {item.title}
+                          </h4>
+                          {item.content.length && (
+                            <ul className="space-y-6 mt-4 mb-10">
+                              {item.content.map((item, index) => (
+                                <li key={index}>{item}</li>
+                              ))}
+                            </ul>
+                          )}
+                        </div>
+                      ))}
+                    </DialogDescription>
+                  </DialogHeader>
+                </DialogContent>
+              </Dialog>
             </li>
-            <li className="font-semibold text-lg flex items-center gap-x-2 cursor-pointer">
-              <img src="/right-arrow.png" className="w-4" /> Privacy
+            <li>
+              <Dialog>
+                <DialogTrigger className="font-semibold text-lg flex items-center gap-x-2 cursor-pointer">
+                  <img src="/right-arrow.png" className="w-4" /> Privacy
+                </DialogTrigger>
+                <DialogContent className="h-[90%] bg-white overflow-y-scroll">
+                  <DialogHeader>
+                    <DialogTitle className="text-black text-start font-bold text-2xl mb-10">
+                      PRIVACY POLICY
+                    </DialogTitle>
+                    <DialogDescription>
+                      {privacyPolicy.slice(0, 9).map((item, index) => (
+                        <div
+                          key={index}
+                          className="text-black text-start h-max"
+                        >
+                          <h4 className="font-semibold">
+                            {index + 1}. {item.title}
+                          </h4>
+                          {item.content.length && (
+                            <ul className="space-y-6 mt-4 mb-10">
+                              {item.content.map((item, index) => (
+                                <li key={index}>{item}</li>
+                              ))}
+                            </ul>
+                          )}
+                        </div>
+                      ))}
+                      <table className="my-10 text-black text-start">
+                        <tr className="border border-black">
+                          <td className="p-4 border-r border-black">
+                            When you register for a Poker Face Account
+                          </td>
+                          <td className="p-4">
+                            Contact Data Identity Data Financial Data
+                            Photographs
+                          </td>
+                          <td className="p-4 border-l border-black">
+                            We process this:
+                            <br />
+                            <br />
+                            • to create and manage your account with us
+                            <br />
+                            • to communicate with you about your account
+                            <br />
+                            <br />
+                            We rely on performance of a contract as the lawful
+                            basis for collecting and using your personal
+                            information.
+                          </td>
+                        </tr>
+                        <tr className="border border-black">
+                          <td className="p-4 border-r border-black">
+                            When we communicate with you about the management of
+                            your TG.Casino Account
+                          </td>
+                          <td className="p-4">
+                            Contact Data Identity Data Financial Data
+                            Transaction Data Technical Data Profile Data
+                            Marketing & Communications Data
+                          </td>
+                          <td className="p-4 border-l border-black">
+                            We process this:
+                            <br />
+                            <br />• to provide you with the best customised
+                            service <br />
+                            • to deal with queries and complaints
+                            <br />
+                            <br />
+                            We rely on performance of a contract as the lawful
+                            basis for collecting and using your personal
+                            information to manage your account. We rely on our
+                            legitimate interest of promoting our products and
+                            services.
+                          </td>
+                        </tr>
+                        <tr className="border border-black">
+                          <td className="p-4 border-r border-black">
+                            When developing our understanding of our customers
+                            to improve our services and website.
+                          </td>
+                          <td className="p-4">
+                            Contact Data Identity Data Transaction Data
+                            Technical Data Profile Data Usage Data Marketing &
+                            Communications Data
+                          </td>
+                          <td className="p-4 border-l border-black">
+                            We process this:
+                            <br />
+                            <br />
+                            • to understand the behaviors and demographics of
+                            our customers
+                            <br />
+                            • to understand our customers needs and interests
+                            <br />
+                            <br />
+                            We rely on our legitimate interest of improving our
+                            business and services
+                          </td>
+                        </tr>
+                        <tr className="border border-black">
+                          <td className="p-4 border-r border-black">
+                            When conducting marketing activity and determining
+                            what products, services and offers may be relevant
+                            to you
+                          </td>
+                          <td className="p-4">
+                            Contact Data Identity Data Transaction Data
+                            Technical Data Profile Data Usage Data Marketing &
+                            Communications Data
+                          </td>
+                          <td className="p-4 border-l border-black">
+                            We rely on our legitimate interests of marketing our
+                            business, services and products To the extent that
+                            we may have obtained your consent that is only
+                            consent to receive electronic marketing
+                            communications it does not change the legitimate
+                            interests basis we are relying on to conduct our
+                            marketing activity
+                          </td>
+                        </tr>
+                        <tr className="border border-black">
+                          <td className="p-4 border-r border-black">
+                            Conducting our regulatory and compliance obligations
+                          </td>
+                          <td className="p-4">
+                            Contact Data Identity Data Financial Data
+                            Transaction Data Technical Data Profile Data Usage
+                            Data
+                          </td>
+                          <td className="p-4 border-l border-black">
+                            We rely on our obligation to comply with a legal
+                            obligation to share information such as with
+                            regulators, insurers, or government agencies. This
+                            includes conducting anti-money laundering, KYC,
+                            anti-fraud checks we may be required to conduct
+                            under applicable laws
+                          </td>
+                        </tr>
+                      </table>
+                      {privacyPolicy.slice(9, 14).map((item, index) => (
+                        <div
+                          key={index}
+                          className="text-black text-start h-max"
+                        >
+                          <h4 className="font-semibold">
+                            {index + 10}. {item.title}
+                          </h4>
+                          {item.content.length && (
+                            <ul className="space-y-6 mt-4 mb-10">
+                              {item.content.map((item, index) => (
+                                <li key={index}>{item}</li>
+                              ))}
+                            </ul>
+                          )}
+                        </div>
+                      ))}
+                    </DialogDescription>
+                  </DialogHeader>
+                </DialogContent>
+              </Dialog>
             </li>
-            <li className="font-semibold text-lg flex items-center gap-x-2 cursor-pointer">
-              <img src="/right-arrow.png" className="w-4" /> Cookies
+            <li>
+              <Dialog>
+                <DialogTrigger className="font-semibold text-lg flex items-center gap-x-2 cursor-pointer">
+                  <img src="/right-arrow.png" className="w-4" /> Cookies
+                </DialogTrigger>
+                <DialogContent className="h-[90%] bg-white overflow-y-scroll">
+                  <DialogHeader>
+                    <DialogTitle className="text-black text-start font-bold text-2xl mb-10">
+                      COOKIES
+                    </DialogTitle>
+                    <DialogDescription>
+                      {cookiesPolicy.map((item, index) => (
+                        <div
+                          key={index}
+                          className="text-black text-start h-max"
+                        >
+                          <h4 className="font-semibold">
+                            {index + 1}. {item.title}
+                          </h4>
+                          {item.content.length && (
+                            <ul className="space-y-6 mt-4 mb-10">
+                              {item.content.map((item, index) => (
+                                <li key={index}>{item}</li>
+                              ))}
+                            </ul>
+                          )}
+                        </div>
+                      ))}
+                    </DialogDescription>
+                  </DialogHeader>
+                </DialogContent>
+              </Dialog>
             </li>
           </ul>
         </div>
@@ -31,8 +261,12 @@ const Footer = () => {
             {Array.from({ length: 5 }).map((_, index) => (
               <img
                 key={index}
-                className="w-8 tablet:w-12"
-                src={`/social-${index + 1}.svg`}
+                className={`${
+                  index === 4 ? "w-12 tablet:w-16" : "w-8 tablet:w-12"
+                }`}
+                src={`/social-${index + 1}.${
+                  index === 4 || index === 2 ? "png" : "svg"
+                }`}
                 alt=""
               />
             ))}
---
+++ data/cookies.ts
@@ -0,0 +1,47 @@
+export const cookiesPolicy = [
+  {
+    title: "Introduction",
+    content: [
+      "Welcome to Poker Face’s Cookie Policy. This policy provides detailed information about how and when we use cookies on our website. By continuing to browse our site, you consent to the use of these cookies in accordance with this policy. As our website is based in Curaçao, our cookie practices are designed to comply with the relevant regulations applicable within the jurisdiction.",
+    ],
+  },
+  {
+    title: "What is a Cookie?",
+    content: [
+      "A cookie is a small piece of data that a website asks your browser to store on your computer or mobile device. It enables the website to remember your actions and preferences (such as login, language, font size, and other display preferences) over a period of time, so you don’t have to keep re-entering them whenever you come back to the site or browse from one page to another.",
+    ],
+  },
+  {
+    title: "How We Use Cookies",
+    content: [
+      "We use cookies on our website:",
+      "• 3.1 Session Cookies: These are temporary cookies that remain only until you close your browser. They help our website recognize you as you move between pages.",
+      "• 3.2 Persistent Cookies: These remain on your device until they expire or you delete them. They help us recognize you when you return to our website and remember your preferences.",
+      "• 3.3 Third-Party Cookies: We may also use third-party cookies to help us with market research, tracking revenue, improving site functionality, and monitoring compliance with our terms and conditions and copyright policy.",
+    ],
+  },
+  {
+    title: "Your Choices",
+    content: [
+      "You have the ability to accept or decline cookies. Most browsers automatically accept cookies, but you can modify your browser setting to decline cookies if you prefer. If you choose to decline cookies, you may not be able to fully experience the interactive features of our website.",
+    ],
+  },
+  {
+    title: "Cookie Management",
+    content: [
+      "If you want to delete cookies from your device at any time, you can use the settings of your web browser. You can also set your browser to block cookies or to warn you before a cookie is stored.",
+    ],
+  },
+  {
+    title: "Changes to this Policy",
+    content: [
+      "From time to time, we may update our Cookie Policy to reflect changes to our cookie practices. If we make any substantial changes, we will notify you by posting a prominent announcement on our website.",
+    ],
+  },
+  {
+    title: "Contact Us",
+    content: [
+      "If you have questions or concerns about this Cookie Policy, please contact us at contact@PokerFace.",
+    ],
+  },
+];
